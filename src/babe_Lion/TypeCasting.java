8779fe14d24f4d488bdb33c06d8b7a89c19df39e
이다온 <ondayary@gmail.com>
2022-10-05 02:34:49 +0900

    형변환

--- src/babe_Lion/TypeCasting.java
+++ src/babe_Lion/TypeCasting.java
@@ -3,7 +3,6 @@ package babe_Lion;
 import java.lang.reflect.Type;
 
 public class TypeCasting {
-
     public void divide(int first, int second) {
         float fFirst = (float)first;
         float fSecond = (float)second;
@@ -12,15 +11,13 @@ public class TypeCasting {
     }
 
     public static void main(String[] args) {
-        // int first = 2;
-        // int second = 3;
-        // int는 소수점이 없습니다. Integer
-        // 소수점 있는 것 : 유리수
-        // TypeCasting Type(형)이 바뀐다.
-        // TypeCasting 형변환
-        // System.out.println(first / second);
-
         TypeCasting tc = new TypeCasting();
         tc.divide(2, 3);
+
+        // int(Integer)는 소수점이 없다.
+        // TypeCasting Type(형)이 바뀐다. => 형변환
+        // int first = 2;
+        // int second = 3;
+        // System.out.println(first / second);
     }
 }
