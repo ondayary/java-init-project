639a0dd6355569b90ad50798c001e66a5f4910c0
이다온 <ondayary@gmail.com>
2022-10-05 02:23:51 +0900

    출력하기 07

---
+++ src/Task/CodeUp_1007.java
@@ -0,0 +1,10 @@
+package Task;
+
+public class CodeUp_1007 {
+    public static void main(String[] args) {
+        // "C:\Download\hello.cpp" 출력
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+
+        // 백슬래시 문자 \ 를 출력하기 위해서는 \\ 로 출력해야 한다.
+    }
+}
