b35f0f6606cba405dbb5e6160f7c43625a97daf4
이다온 <ondayary@gmail.com>
2022-10-05 02:23:29 +0900

    출력하기 04

---
+++ src/Task/CodeUp_1004.java
@@ -0,0 +1,11 @@
+package Task;
+
+public class CodeUp_1004 {
+    public static void main(String[] args) {
+        // 'Hello' 출력
+        System.out.println("'Hello'");
+
+        // System.out.println("\'Hello\'");
+        // 따옴표 문자 ' 를 출력하기 위해서는 \' 로 입력해야 한다.
+    }
+}
