70229aabac5ecd1fcb407eaeaf543ddf7d69447f
이다온 <ondayary@gmail.com>
2022-10-05 02:35:24 +0900

    배열을 활용한 계산

--- src/babe_Lion/VariableExercise.java
+++ src/babe_Lion/VariableExercise.java
@@ -2,15 +2,14 @@ package babe_Lion;
 
 public class VariableExercise {
     public static void main(String[] args) {
+        // 50000 ~ 10 원까지 배열에 넣어서 출력
         /*
-        // 50000 ~ 10 원까지 배열에 넣어보세요.
         int[] money = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10};
 
         for(int i = 0; i < 8; i++) {
             System.out.printf("%d\n", money[i]);
         }
         */
-
 
         // 계산기
         // arr 0번 + arr 1번  --- 1번 계산
