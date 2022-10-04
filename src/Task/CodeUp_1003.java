99f47d0edadec38bfc15c8ce0595a4c8432651c4
이다온 <ondayary@gmail.com>
2022-10-05 02:23:18 +0900

    출력하기 03

---
+++ src/Task/CodeUp_1003.java
@@ -0,0 +1,9 @@
+package Task;
+
+public class CodeUp_1003 {
+    public static void main(String[] args) {
+        // Hello
+        // World 출력
+        System.out.println("Hello\nWorld");
+    }
+}
