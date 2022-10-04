8b69983bb3f39c6bd5323d6c7147207af4ed7cf8
이다온 <ondayary@gmail.com>
2022-10-05 02:24:25 +0900

    정수 1개 입력받아 그대로 출력

---
+++ src/Task/CodeUp_1010.java
@@ -0,0 +1,13 @@
+package Task;
+
+import java.util.Scanner;
+
+public class CodeUp_1010 {
+    public static void main(String[] args) {
+        // 정수형(int)으로 변수를 선언하고, 변수에 정수값을 저장한 후
+        // 변수에 저장되어 있는 값을 그대로 출력해보자.
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
