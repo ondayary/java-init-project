9637823c74a45fb7a723933b743365e2c7bbbddf
이다온 <ondayary@gmail.com>
2022-10-05 02:31:08 +0900

    반복문 출력

--- src/babe_Lion/ForStatement.java
+++ src/babe_Lion/ForStatement.java
@@ -1,28 +1,24 @@
-
 package babe_Lion;
 
 public class ForStatement {
-
     public static void main(String[] args) {
-
         // 반복문
         // 1 ~ 5 까지 출력
         for(int i = 1; i < 6; i+=1) { // i+=1 대신 i++
             System.out.print(i + " ");
         }
-        System.out.println();
+            System.out.println();
 
-        // 2, 4, 6, 8, 10
+        // 2, 4, 6, 8, 10 출력
         for(int i = 2; i <= 10; i+=2) {
             System.out.print(i + " ");
         }
 
+        // 다른 방법
         /*
         for(int i = 1; i <= 5; i+=1) {
             System.out.print(i * 2);
         }
         */
-
     }
-
 }
