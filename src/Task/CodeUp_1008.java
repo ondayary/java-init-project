63226fa445ae8b6646708866a16b5365786ffb5e
이다온 <ondayary@gmail.com>
2022-10-05 02:24:01 +0900

    출력하기 08

---
+++ src/Task/CodeUp_1008.java
@@ -0,0 +1,13 @@
+package Task;
+
+public class CodeUp_1008 {
+    public static void main(String[] args) {
+        // 유니코드를 이용하여 출력
+        // ┌┬┐
+        // ├┼┤
+        // └┴┘
+        System.out.println("\u250C\u252C\u2510");
+        System.out.println("\u251C\u253C\u2524");
+        System.out.println("\u2514\u2534\u2518");
+    }
+}
